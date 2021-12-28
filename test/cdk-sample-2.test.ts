f08a75328eed1d7abdfc0fa5549010a8d1878b9a
Adriano Sastre Vieira <adrianosastre@inatel.br>
2021-12-28 09:13:17 -0300

    Initial commit

---
+++ test/cdk-sample-2.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as CdkSample2 from '../lib/cdk-sample-2-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/cdk-sample-2-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new CdkSample2.CdkSample2Stack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
